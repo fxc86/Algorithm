6fe3d57be2685c206a88f513999400d6d1adabee
fxc86 <fengxuecheng11@163.com>
2018-05-11 13:51:57 +0800

    ss

---
+++ bubbleSort.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * ########################
+ * php ÊµÏÖÃ°ÅİÅÅĞòËã·¨
+ * @author http://phpff.com  2014/12/29
+ */
+function bubbleSort($numbers) {
+    $cnt = count($numbers);
+    for ($i = 0; $i < $cnt; $i++) {
+        for ($j = 0; $j < $cnt - $i - 1; $j++) {
+            if ($numbers[$j] > $numbers[$j + 1]) {
+                $temp = $numbers[$j];
+                $numbers[$j] = $numbers[$j + 1];
+                $numbers[$j + 1] = $temp;
+            }
+        }
+		echo implode(",",$numbers)."\n";
+    }
+ 
+    return $numbers;
+}
+ 
+$num = array(20, 40, 60, 80, 30, 70, 90, 10, 50, 0);
+print_r(bubbleSort($num));
